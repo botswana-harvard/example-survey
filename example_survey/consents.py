e670bec8ef42ff365acf46c33da3d486df4ffb54
erikvw <ew2789@gmail.com>
2017-01-06 03:27:09 +0200

    update start/end dates

--- example_survey/consents.py
+++ example_survey/consents.py
@@ -1,3 +1,7 @@
+import arrow
+
+from dateutil.relativedelta import relativedelta
+
 from edc_consent.consent import Consent
 from edc_consent.site_consents import site_consents
 from edc_constants.constants import MALE, FEMALE
@@ -9,8 +13,8 @@ app_config = django_apps.get_app_config('edc_protocol')
 subjectconsent_v1 = Consent(
     'example_survey.subjectconsent',
     version='1',
-    start=app_config.study_open_datetime,
-    end=app_config.study_close_datetime,
+    start=arrow.utcnow().floor('hour') - relativedelta(years=1),
+    end=arrow.utcnow().ceil('hour'),
     age_min=16,
     age_is_adult=18,
     age_max=64,
